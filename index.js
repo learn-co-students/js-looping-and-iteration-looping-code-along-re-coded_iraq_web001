51f897b56cb22c87e659e5d38109322f9514f0d9
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-05-12 11:16:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+const arryaName=('Lisa', 'Kaitlin', 'Jan'], "birthday");
+function writeCards(arryaNAme,eventName){
+  for(let i=0; i<=arryaNAme.length; i++){
+    console.log("Thank you,".concat(writeCards, ", for the wonderful birthday gift!"));
+  }
+  return(arryaNAme);
+}

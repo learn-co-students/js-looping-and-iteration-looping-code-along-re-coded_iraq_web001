eb645e936d4d1f4b608d70d6b7de9f1e1de067c4
Evan hameed <evanhameed9966@gmail.com>
2020-05-12 12:07:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+
+
+function writeCards(names,eventName){
+for( let i=0;i<=names.length-1;i++){
+
+  names[i] =  `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+}
+return names;
+}
+writeCards(["Lisa","Kaitlin","Jan"],"surprise");
+
+
+
+function countDown(positiveInt){
+
+while(positiveInt>=0){
+console.log(positiveInt);
+positiveInt--;
+
+}
+
+}
+countDown(10);

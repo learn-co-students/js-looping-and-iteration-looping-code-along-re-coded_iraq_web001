71f7d4bf9e4eac92707e0b510848af3443a02d13
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-13 04:54:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const x =['surprise'];
+
+
+function writeCards ( y){
+  let z =[];
+  for( let i =0 ; i< y.length;i++){
+    z.push(`Thank you, ${(y[i])}, for the wonderful ${(x)} gift!`);
+  }
+  return z
+}
+
+function countDown(w){
+while(w>=0){
+  console.log(w);
+  w--;
+}
+}

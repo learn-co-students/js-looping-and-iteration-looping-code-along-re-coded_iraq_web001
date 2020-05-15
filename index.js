1860a21f0aa929ce199d5a9f137497f2efefddfb
Abdulrahman Maher <ibn.maher.96@gmail.com>
2020-05-15 16:10:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const para =['surprise'];
+function writeCards (name){
+  let text =[];
+  for( let i =0 ; i < name.length; i++){
+   text .push(`Thank you, ${(name[i])}, for the wonderful ${(para)} gift!`);}
+  return text
+}
+function countDown(number){
+while(number>=0){
+  console.log(number);
+  number--;}
+}

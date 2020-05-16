890221ace289f43815e829c181b66efa3e6dbfd4
jalal Arif <jalalharfjalal@gmail.com>
2020-05-16 18:59:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+
+
+function writeCards(cards,gifts) {
+  const array = [];
+  for (let i = 0; i < cards.length; i++) {
+    array.push(`Thanks, ${cards[i]}, for the wonderful ${gifts}your gift!`);
+  }
+
+  return array;
+}
+
+writeCards([ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise');
+
+
+
+function countDown(num) {
+while(num >= 0){
+  console.log(num);
+  num--;
+}
+
+}
+
+
+countDown(10);
+
+

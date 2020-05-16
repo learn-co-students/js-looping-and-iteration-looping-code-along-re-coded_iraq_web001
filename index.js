0de595d460fc3d1ce322f4cfd6ff38aff77ee223
Bawan Majid <bawanmajid23@gmail.com>
2020-05-16 08:57:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+// Code your solutions in this file
+
+
+
+function writeCards(arr,surprise){
+
+let thankYou=[];
+
+  for(let i=0;i<arr.length;i++){
+  thankYou[i]="Thank you, " +arr[i]+`, for the wonderful ${surprise} gift!`;
+  }
+  return thankYou;
+}
+
+writeCards(["Lisa","Kaitlin","Jan"],"surprise");
+
+function countDown(num){
+  
+  while(num>=0){
+    console.log(num);
+    num--;
+  }
+}
+
+countDown(10);

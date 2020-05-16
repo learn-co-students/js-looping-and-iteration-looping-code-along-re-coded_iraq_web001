dc2329098b227c75099259ca6e1d2793162fec88
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-16 02:27:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, eventName){
+  let msg=[];
+  for(let a=0;a<names.length;a++){
+    msg[a] = "thank you, " +names[a]+ ", for the wonderful " +eventName+ " gift!"
+  }
+  return msg;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(positiveInt){
+  while(positiveInt>=0){
+  console.log(positiveInt);
+  positiveInt--;
+}
+}
+
+countDown(10);

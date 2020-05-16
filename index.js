09ba2e656b46016cfd542e0c4be3a4b446e74384
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-05-16 14:23:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function writeCards(name,events){
+  let thank=[];
+  for(let i=0;i<name.length;i++){
+    thank.push(`Thank you, ${name[i]}, for the wonderful ${events} gift!`);
+
+  }
+  return thank;
+}
+writeCards(["Ada", "Brendan", "Ali"], "surprise");
+
+let countup = 10;
+function countDown(countup){
+while (countup >= 0 ) {
+  console.log(countup--);
+}
+return countup;
+}
+countDown(countup)

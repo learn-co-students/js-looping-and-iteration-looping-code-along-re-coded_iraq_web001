6159268ec8b89786b2442221cc85c1223712269d
zeena kareem <zena34269@gmail.com>
2020-05-18 11:07:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+
+Function writeCards(names,birthday){
+  const newarray=[];
+  for(let i=0;i<names.length;i++){
+    newarray[i]=`Thank you, + ${names[i]} +, for the wonderful + ${ birthday}+ gift!`;
+  }
+  return newarray
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "surprise");
+countDown(10);

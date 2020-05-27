ed443a6f7bad6836f61e6b5db20dc1a861b59ad4
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-27 00:32:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const arr=['Lisa', 'Kaitlin', 'Jan'];
+function writeCards(arr, surprise){
+  for(let i=0;i<arr.length;i++)
+  {
+    arr[i]= `Thank you, ${arr[i]}, for the wonderful ${surprise} gift!`;
+  }
+  return arr;
+}
+writeCards(arr,"surprise");
+
+function countDown (integer){
+while(integer >=0){
+ console.log(integer--);
+}
+}
+countDown(10);
+
+
+

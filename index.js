ee38cff4428bb65e196ed18209420ebe4efa5ffd
zena abd alkarem alhello <zena34269@gmail.com>
2020-08-09 19:49:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 
-Function writeCards(names,birthday){
-  const newarray=[];
-  for(let i=0;i<names.length;i++){
-    newarray[i]=`Thank you, + ${names[i]} +, for the wonderful + ${ birthday}+ gift!`;
-  }
-  return newarray
+function writeCards(name , event){
+  let arr=[];
+  for(let i=0;i<name.length;i++)
+  {arr.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);}
+  return arr;
+  
 }
 
-writeCards(["Ada", "Brendan", "Ali"], "surprise");
-countDown(10);
+
+writeCards(["Lisa", "Kaitlin", "Jan"], "birthday");
+
+function countDown(num){
+ while(num>=0){
+    console.log(num);
+    num--;
+    
+  } 
+}
